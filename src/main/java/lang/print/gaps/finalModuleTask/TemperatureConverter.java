d1e8aa2be881f83f5f5f7d30f9469506231b6754
macservis <fboqiyev1065@gmail.com>
2023-03-10 15:19:14 +0500

    stage0-module3-taskFinal updated

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float a = temperatureCelsius;
+        float f = (a * 9)/5 + 32;
+        System.out.println(f);
     }
 }
